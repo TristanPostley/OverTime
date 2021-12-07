73652fb8f53624d7d439424b3372d4c593e22032
Tristan Postley <tpostley@gmail.com>
2021-12-07 09:49:52 -0800

    initial

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OverTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 0;
+            Console.WriteLine("Hello, to calculate overtime pay please input a start time after 9, an end time, a pay rate, and an overtime multiplier.");
+            Console.WriteLine("Start time: ");
+            double startTime = double.Parse(Console.ReadLine());
+            Console.WriteLine("End time: ");
+            double endTime = double.Parse(Console.ReadLine());
+            Console.WriteLine("Pay rate: ");
+            double payRate = double.Parse(Console.ReadLine());
+            Console.WriteLine("multiplier: ");
+            double multiplier = double.Parse(Console.ReadLine());
+
+            if(endTime <= 17)
+            {
+                total = (endTime - startTime) * payRate;
+            }
+            else 
+            {
+                double basePay = (17 - startTime) * payRate;
+                double overPay = (endTime - 17) * payRate * multiplier;
+                total = basePay + overPay;
+            }
+
+            Console.WriteLine($"Your total pay is: ${total.ToString("F")}");
+        }
+    }
+}
